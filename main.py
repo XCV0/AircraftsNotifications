c9e486c1729a7c8baf3ca113c080c02b5e43136c
XCV0 <frt.mrartem@gmail.com>
2022-08-24 09:03:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,9 +19,30 @@ def check_connection(url, method):
 
 
 def dump1090(notifi_type):
+    last_aircrafts = []
     req_link = f"http://{ip_addres}:{port}/data.json"
     if False == check_connection(req_link, "get"):
         print(f"Site {req_link} is now available")
+        
+    while True:
+        try:
+            request = requests.post(req_link)
+
+            json_aircrafts_list = json.loads(request.text)["acList"]
+
+            if last_aircrafts != json_aircrafts_list:
+                if json_aircrafts_list != []:
+                    last_aircrafts = json_aircrafts_list
+                    if notifi_type == "sound":
+                        playsound("1.mp3")
+                    else:
+                        print("! AIRCRAFT FIND")
+
+            time.sleep(2)
+
+        except:
+            print("ERROR, CONTINUE")
+            time.sleep(5)
 
 
 def Virtualradar(notifi_type):
@@ -30,6 +51,7 @@ def Virtualradar(notifi_type):
     if False == check_connection(req_link, "post"):
         print(f"Site {req_link} is now available")
         exit()
+    
 
     while True:
         try:
@@ -44,7 +66,7 @@ def Virtualradar(notifi_type):
                         playsound("1.mp3")
                     else:
                         print("! AIRCRAFT FIND")
-                        
+
             time.sleep(2)
 
         except:
